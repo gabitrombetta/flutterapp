13d48cf0ee697cd674c496cb84391ecf44ae2a43
Gabriela Trombetta <gabrielatrombetta20@gmail.com>
2025-09-10 14:47:29 -0300

    add files to remote repository

---
+++ lib/components/menu.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class Menu extends StatefulWidget {
+  const Menu({super.key});
+
+  @override
+  State<Menu> createState() => _MenuState();
+}
+
+class _MenuState extends State<Menu> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(),
+            child: Text('Drawer Header'),
+          ),
+          ListTile(
+            title: const Text('Item 1'),
+            onTap: () {
+              // Update the state of the app.
+              // ...
+            },
+          ),
+          ListTile(
+            title: const Text('Item 2'),
+            onTap: () {
+              // Update the state of the app.
+              // ...
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
